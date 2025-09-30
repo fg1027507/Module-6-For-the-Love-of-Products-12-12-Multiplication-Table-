79e6e26acc8e0412910a4b047cbc947c39de8615
fg1027507 <fg1027507@otc.edu>
2025-09-30 14:01:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+For The Love of Products
+Created by finn gilbert on 9/30/2025
+*/
+using System.Globalization;
+// Set the maximum value for the multiplication table
+int max = 12;
+// Outer loop controls the rows 1 through max
+for (int i = 1; i <= max; i++)
+{
+    // Inner loop controls the columns 1 through max
+    for (int j = 1; j <= max; j++)
+    {
+        // Multiply row number i by column number j to get the product
+        int ResultColumn = j * i;
+        // Print the product inside vertical bars with tab spacing for alignment
+        Console.Write("|" + ResultColumn + "|" + "\t");
+        // When the last column is reached, move to a new line and print a separator
+        if (j == 12)
+        {
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
+// Indicate the program has finished running
+Console.WriteLine("Program ended");
